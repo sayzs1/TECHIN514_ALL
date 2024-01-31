1ee6a286e582b9d4418d82976772b9ad3403c9c5
ROIM1998 <bowen98@uw.edu>
2024-01-30 21:57:39 -0800

    Fulfill the 4-stage battery consumption exp

--- Lab5_power-management/wifi-demo/src/main.cpp
+++ Lab5_power-management/wifi-demo/src/main.cpp
@@ -7,10 +7,10 @@
 #include "addons/RTDBHelper.h"
 
 const char* ssid = "UW MPSK";
-const char* password = "12345678"; // Change to your network password
-#define DATABASE_URL "https://xxxxx.firebaseio.com/" // Change to your Firebase database URL
-#define API_KEY "xxxxx" // Change to your Firebase database API key
-#define UPLOAD_INTERVAL 15000 // 15 seconds each upload
+const char* password = "YourPasswordHere"; // Replace with your network password
+#define DATABASE_URL "https://xxxxx.firebaseio.com/" // Replace with your database URL
+#define API_KEY "YourAPIKeyHere" // Replace with your API key
+#define UPLOAD_INTERVAL 1000 // 1 seconds each upload
 
 //Define Firebase Data object
 FirebaseData fbdo;
@@ -22,11 +22,85 @@ unsigned long sendDataPrevMillis = 0;
 int count = 0;
 bool signupOK = false;
 
+// HC-SR04 Pins
+const int trigPin = 2;
+const int echoPin = 3;
+
+// Define sound speed in cm/usec
+const float soundSpeed = 0.034;
+
+// Function prototypes
+float measureDistance();
+void connectToWiFi();
+void initFirebase();
+void sendDataToFirebase(float distance);
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  while(!Serial);
-  delay(1000);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  // First, we start with the ultrasonic sensor only
+  Serial.println("Measuring distance for 5 seconds...");
+  unsigned long startTime = millis();
+  while (millis() - startTime < 5000)
+  {
+    measureDistance();
+    delay(100); // Delay between measurements
+  }
+
+  // Now, turn on WiFi and keep measuring
+  Serial.println("Turning on WiFi and measuring for 5 seconds...");
+  connectToWiFi();
+  startTime = millis();
+  while (millis() - startTime < 5000)
+  {
+    measureDistance();
+    delay(100); // Delay between measurements
+  }
+
+  // Now, turn on Firebase and send data every 1 second with distance measurements
+  Serial.println("Turning on Firebase and sending data every 1 second...");
+  initFirebase();
+  startTime = millis();
+  while (millis() - startTime < 5000)
+  {
+    float currentDistance = measureDistance();
+    sendDataToFirebase(currentDistance);
+    delay(100); // Delay between measurements
+  }
+
+  // Go to deep sleep for 5 seconds
+  Serial.println("Going to deep sleep for 5 seconds...");
+  esp_sleep_enable_timer_wakeup(5 * 1000000); // 5 seconds
+  esp_deep_sleep_start();
+}
+
+void loop(){
+  // This is not used
+}
+
+float measureDistance()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  float distance = duration * soundSpeed / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  return distance;
+}
+
+void connectToWiFi()
+{
   // Print the device's MAC address.
   Serial.println(WiFi.macAddress());
   WiFi.begin(ssid, password);
@@ -38,7 +112,10 @@ void setup() {
   Serial.println("Connected to WiFi");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+}
 
+void initFirebase()
+{
   /* Assign the api key (required) */
   config.api_key = API_KEY;
 
@@ -58,39 +135,24 @@ void setup() {
   config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
   
   Firebase.begin(&config, &auth);
-  Firebase.reconnectWiFi(true);
+  Firebase.reconnectNetwork(true);
 }
 
-void loop(){
-  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > UPLOAD_INTERVAL || sendDataPrevMillis == 0)){
+void sendDataToFirebase(float distance){
+    if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > UPLOAD_INTERVAL || sendDataPrevMillis == 0)){
     sendDataPrevMillis = millis();
-    // Write an Int number on the database path test/int
-    if (Firebase.RTDB.pushInt(&fbdo, "test/int", count)){
+    // Write an Float number on the database path test/float
+    if (Firebase.RTDB.pushFloat(&fbdo, "test/distance", distance)){
       Serial.println("PASSED");
       Serial.print("PATH: ");
       Serial.println(fbdo.dataPath());
       Serial.print("TYPE: " );
       Serial.println(fbdo.dataType());
-    }
-    else {
+    } else {
       Serial.println("FAILED");
       Serial.print("REASON: ");
       Serial.println(fbdo.errorReason());
     }
     count++;
-    
-    // Write an Float number on the database path test/float
-    if (Firebase.RTDB.pushFloat(&fbdo, "test/float", 0.01 + random(0,100))){
-      Serial.println("PASSED");
-      Serial.print("PATH: ");
-      Serial.println(fbdo.dataPath());
-      Serial.print("TYPE: " );
-      Serial.println(fbdo.dataType());
-    }
-    else {
-      Serial.println("FAILED");
-      Serial.print("REASON: ");
-      Serial.println(fbdo.errorReason());
-    }
   }
 }
